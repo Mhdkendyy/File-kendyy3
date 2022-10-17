28308389c8ebf3d7c4bcef0111cc5b5620652673
Mhdkendyy <114293906+Mhdkendyy@users.noreply.github.com>
2022-10-17 19:24:44 +0800

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -27,9 +27,7 @@ async def is_subscribed(filter, client, update):
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL2, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL3, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL4, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL5, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL6, user_id=user_id)     
+             
     except UserNotParticipant:
         return False
 
